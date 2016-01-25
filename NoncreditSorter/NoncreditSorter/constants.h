2f3c8a675888d338ff181ecf15574e6860703cc8
kevin <kevinfserrano@gmail.com>
2016-01-25 13:07:45 -0800

    Removed the precompiled header from noncredit.cpp

---
+++ NoncreditSorter/NoncreditSorter/constants.h
@@ -0,0 +1,40 @@
+#pragma once
+
+enum Status
+{
+	OPEN,
+	CLOSE,
+	CANCELED,
+	ERROR_STATUS
+};
+
+enum Semester
+{
+	WINTER,
+	SPRING,
+	SUMMER,
+	FALL,
+	ERROR_SEMESTER
+};
+
+enum Session
+{
+	FULL,
+	FIRST,
+	SECOND,
+	ERROR_SESSION
+};
+
+const int EVENING_HOURS = 16; //after 4pm
+const int ASCII_INTEGER_CONVERTER = 48;
+const int TIME_CHARACTERS = 5;
+
+int const CHAR0 = 48;
+int const CHAR9 = 57;
+
+const int NUM_OF_OUTPUT_FILES = 14;
+
+
+
+
+
